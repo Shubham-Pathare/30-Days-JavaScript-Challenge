ef7c8af66e03975a531d517b48af24e78a462ac8
Shubham_Pathare <141949055+Shubham-Pathare@users.noreply.github.com>
2024-07-22 15:29:45 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,40 @@
+// Activity 1 : Variable Declaration
+// Task1
+var num = 1;
+console.log(num);
+
+// Task2
+let userName = "Ironman";
+console.log(userName);
+
+// Activity 2 : Constant Declaration
+// TAsk 3
+const isLoggedIn = false;
+console.log(isLoggedIn);
+
+// Activity 3 : Data Types
+// Task 4
+const number = 3;
+const username = "Ironman";
+const password = true;
+const obj = { name: "Ironman", age: "100", email: "Ironman@mail.com" };
+const arr = [1, 2, 3, 4, 5];
+
+console.log(typeof number);
+console.log(typeof username);
+console.log(typeof password);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity 4 : Reassigning Variables
+//task 5
+let initialValue = 2;
+console.log(initialValue);
+initialValue = 3;
+console.log(initialValue);
+
+// Activity 5 : Understanding
+//Task 6
+const value = 10;
+console.log(value);
+// value = 20;  TypeError : Assignment to constant variable.
